b308f3e3d1024284c66e45512b9c9aee536dbb7e
seonjuleee <nlsj0820@naver.com>
2023-12-03 17:44:54 +0900

    Feat: item 유효성 검사

--- item-service/src/main/java/com/seonjuleee/msa/itemservice/controller/ItemController.java
+++ item-service/src/main/java/com/seonjuleee/msa/itemservice/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.seonjuleee.msa.itemservice.controller;
 import com.seonjuleee.msa.itemservice.dto.ItemDTO;
 import com.seonjuleee.msa.itemservice.dto.ResponseDTO;
 import com.seonjuleee.msa.itemservice.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class ItemController {
     private final ItemService itemService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ResponseEntity<ResponseDTO> add(@RequestBody ItemDTO itemDTO) {
+    public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO) {
         ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
 
         itemService.insertItem(itemDTO);
--- item-service/src/main/java/com/seonjuleee/msa/itemservice/dto/ItemDTO.java
+++ item-service/src/main/java/com/seonjuleee/msa/itemservice/dto/ItemDTO.java
@@ -1,13 +1,25 @@
 package com.seonjuleee.msa.itemservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ItemDTO {
+    @NotBlank(message = "ID는 필수 입력 값입니다.")
+    @Size(max = 10, message = "ID는 크기 10 이하까지 작성 가능합니다.")
     private String id;
+
+    @Size(max = 20, message = "이름은 20자까지 작성 가능합니다.")
     private String name;
+
+    @Size(max = 200, message = "설명은 최대 200자까지 작성 가능합니다.")
     private String description;
+
+    @Positive
     private long count;
+
     private String regDts; // 물품의 최초등록일시
     private String updDts; // 물품의 최종수정일시
 }
