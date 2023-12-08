7e19d888724844630393e9abe3cdf72f4d206120
seonjuleee <nlsj0820@naver.com>
2023-12-08 14:30:57 +0900

    Feat: SpringDoc OpenApi 적용

--- item-service/src/main/java/com/seonjuleee/msa/itemservice/controller/ItemController.java
+++ item-service/src/main/java/com/seonjuleee/msa/itemservice/controller/ItemController.java
@@ -6,6 +6,11 @@ import com.seonjuleee.msa.itemservice.dto.ResponseDTO;
 import com.seonjuleee.msa.itemservice.exception.ApiException;
 import com.seonjuleee.msa.itemservice.service.ItemService;
 import com.seonjuleee.msa.itemservice.valid.ItemTypeValid;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@OpenAPIDefinition(info = @Info(title = "물품 처리요청 API", description = "물품 처리요청 API", version = "v1"))
 @RestController
 @RequestMapping(value = "v1/item")
 @Slf4j
@@ -22,6 +28,11 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @Operation(summary = "물품 등록 요청", description = "물품 등록을 진행할 수 있다.", tags = { "addItem" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "SUCCESS"),
+            @ApiResponse(responseCode = "501", description = "API EXCEPTION")
+    })
     @RequestMapping(value = "/add/{itemType}", method = RequestMethod.POST)
     public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO,
                                            @ItemTypeValid @PathVariable String itemType) throws Exception {
--- item-service/src/main/java/com/seonjuleee/msa/itemservice/dto/ItemDTO.java
+++ item-service/src/main/java/com/seonjuleee/msa/itemservice/dto/ItemDTO.java
@@ -1,5 +1,6 @@
 package com.seonjuleee.msa.itemservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -7,21 +8,29 @@ import lombok.Data;
 
 @Data
 public class ItemDTO {
+    @Schema(description = "물품ID", example = "TESTID")
     @NotBlank(message = "ID는 필수 입력 값입니다.")
     @Size(max = 10, message = "ID는 크기 10 이하까지 작성 가능합니다.")
     private String id;
 
+    @Schema(description = "물품명", example = "과일")
     @Size(max = 20, message = "이름은 20자까지 작성 가능합니다.")
     private String name;
 
+    @Schema(description = "물품설명", example = "물품설명테스트")
     @Size(max = 200, message = "설명은 최대 200자까지 작성 가능합니다.")
     private String description;
 
+    @Schema(description = "물품유형", example = "F : 음식, C : 옷")
     private String itemType;
 
+    @Schema(description = "물품개수", example = "100")
     @Positive
     private long count;
 
+    @Schema(hidden = true)
     private String regDts; // 물품의 최초등록일시
+
+    @Schema(hidden = true)
     private String updDts; // 물품의 최종수정일시
 }
