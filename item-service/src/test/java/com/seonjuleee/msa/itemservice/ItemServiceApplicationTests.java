7d76252e41bb0e373d35cffa1d74efa5f7ef23d1
seonjuleee <nlsj0820@naver.com>
2024-01-21 09:36:34 +0900

    Test: 물품 등록 Test

--- item-service/src/test/java/com/seonjuleee/msa/itemservice/ItemServiceApplicationTests.java
+++ item-service/src/test/java/com/seonjuleee/msa/itemservice/ItemServiceApplicationTests.java
@@ -1,13 +1,70 @@
 package com.seonjuleee.msa.itemservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.seonjuleee.msa.itemservice.controller.ItemController;
+import com.seonjuleee.msa.itemservice.dto.ItemDTO;
+import com.seonjuleee.msa.itemservice.service.ItemService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
 class ItemServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @InjectMocks
+    private ItemController itemController;
+
+    @Mock
+    private ItemService itemService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(itemController)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
     }
 
+    @Test
+    @DisplayName("물품 등록 테스트")
+    void add_item() throws Exception {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            mockMvc.perform(post("/v1/item/add/C")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("accountId", "admin")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(getTestItem()))
+                    )
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    private ItemDTO getTestItem() {
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setId("test1");
+        itemDTO.setName("상품명");
+        itemDTO.setDescription("상품 설명");
+        itemDTO.setCount(50);
+        return itemDTO;
+    }
 }
